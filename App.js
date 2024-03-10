045ce0f720165e3c1be4c8f40fd02a5a12b85870
JordanPinckney <157853781+JordanPinckney@users.noreply.github.com>
2024-03-10 13:21:26 -0400

    Updated App.js

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import MainMenu from './main_forDropDown.js';
-import Individual from './Individual.js';
-import Org_menu from './org_menu.js';
-import V_with_menu from './v_with_app.js'
-import Select_Policy_Ind from './select_policy.js';
+import MainMenu from './components/main_forDropDown.js';
+import Individual from './components/Individual.js';
+import Org_menu from './components/organization_using_app/org_menu.js';
+import V_with_menu from './components/volunteer_org_using_app/v_with_app.js'
+import Select_Policy_Ind from './components/individual/select_policy.js';
+import Signin from './components/SignIn.js';
+import Create_Account from './components/create_account.js';
+import Make_Cleanup_Live_Org from './components/organization_using_app/make_cleanup_live.js';
+
 
 
 const Stack = createStackNavigator();
@@ -14,12 +18,15 @@ const Stack = createStackNavigator();
 export default function App() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Main Menu">
+        <Stack.Navigator initialRouteName="Sign In">
+          <Stack.Screen name="Sign In" component={Signin} />
+          <Stack.Screen name="Create Account" component={Create_Account} />
           <Stack.Screen name="Main Menu" component={MainMenu} />
           <Stack.Screen name="Individual/Volunteer with organization not using app" component={Individual} />
           <Stack.Screen name="Organization" component={Org_menu} />
           <Stack.Screen name="Volunteer with organization using app" component={V_with_menu} />
           <Stack.Screen name="Policy Priority item selection" component={Select_Policy_Ind} />
+          <Stack.Screen name="Make Cleanup Live" component={Make_Cleanup_Live_Org} />
           {/* Other screens */}
         </Stack.Navigator>
       </NavigationContainer>
