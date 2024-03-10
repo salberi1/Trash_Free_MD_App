ba1ac7058b549086189f2d42fff48e52a281e23d
JordanPinckney <157853781+JordanPinckney@users.noreply.github.com>
2024-03-10 13:24:49 -0400

    Updated SignIn.js
    It's now the first screen instead of the main menu

--- SignIn.js
+++ SignIn.js
@@ -1,7 +1,7 @@
-import { View, Text, StyleSheet,TextInput,Pressable } from 'react-native'
+import { View, Text, StyleSheet,TextInput,Pressable, TouchableOpacity, SafeAreaView } from 'react-native'
 import React, { useState } from 'react'
 
-export default function Signin()
+export default function Signin({navigation})
 {
     const [email, setEmail] = useState('email');
     const [password,setPassword] = useState('password');
@@ -11,23 +11,40 @@ export default function Signin()
             <Text style = {styles.boldText}>Email:</Text>
             <TextInput 
             style = {styles.input}
-            placeholder='Enter Email'
+            placeholder='Enter Email...'
+            onChangeText={(text) => setEmail(text)}
             />
 
           <Text style = {styles.boldText}>Password:</Text>
             <TextInput 
             style = {styles.input}
-            placeholder='Enter Password'
+            placeholder='Enter Password...'
+            secureTextEntry={true}
+            onChangeText={(text) => setPassword(text)}
             />
-            <Text>email:{email} password:{password}</Text>
-            
             <View>
-            <Pressable style = {styles.button}>
-              <Text>
-                Submit
+            <TouchableOpacity style = {styles.button}>
+              <Text
+                style={styles.text}
+              >
+                SUBMIT
               </Text>
-            </Pressable>
+            </TouchableOpacity>
             </View>
+            <TouchableOpacity>
+                <Text style={styles.forgot}>
+                Forgot Username/Password
+                </Text>
+              </TouchableOpacity>
+            <TouchableOpacity
+            onPress={() => navigation.navigate("Create Account")}>
+              <Text>create account</Text> 
+            </TouchableOpacity>
+            <TouchableOpacity
+            onPress={() => navigation.navigate("Main Menu")}>
+              <Text>continue as guest</Text>
+              
+            </TouchableOpacity>
         </View>
     )
 }
@@ -38,29 +55,42 @@ const styles = StyleSheet.create({
       backgroundColor: '#afd8c9',
       alignItems: 'center',
       justifyContent: 'center',
-      
-  
     },
-
     input:{
         borderWidth:1,
         borderColor: '#777',
         padding:8,
         margin:10,
         width:200,
-        backgroundColor: '#efca66'
+        backgroundColor: '#fff',
+        borderRadius: 50
     },
-
     boldText:{
       fontWeight: 'bold',
       fontSize: 18
     },
-
     button:{
+        marginTop: 20,
         borderWidth: 1,
-        padding: 50,
+        height: 40,
+        width: 150,
         borderColor: '#777',
-    }
+        backgroundColor: '#efca66',
+        borderRadius: 10
+    },
+    text: {
+      justifyContent: 'center',
+      alignSelf: 'center',
+      marginTop: 5,
+      fontSize: 20,
+      marginBottom: 5
+    },
+    forgot: {
+      marginTop: 5,
+      color: 'gray',
+      fontSize: 12
+    }      
+});
     
 
 });
