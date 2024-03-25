45a243254b282ef76be4042dde3c364e49b15024
jorgeVerduzco <jorgeverduzco626@gmail.com>
2024-03-25 16:22:13 -0400

    Update SignIn.php
    Added comments and fixed some syntax errors

--- SignIn.php
+++ SignIn.php
@@ -5,13 +5,22 @@ $UserEmail = $decodedData['Email'];
 $UserPW = ($decodedData['Password']);
 
 $SQL = "SELECT * FROM newuser WHERE UserEmail = '$UserEmail'";
+
+//creates a query to search for the email the user inputs
 $exeSQL = mysqli_query($conn,$SQL);
+
+//this line sets checkEmail to the number of rows passed through to find the index of email
 $checkEmail = mysqli_num_rows($exeSQL);
 
+/*in the case checkEmail is zero the email is not in the database so no account is associated with it.
+  In the case the email is inside the data base it fetches the row the email is in and checks if the password
+  the user provided matches the one in the database. The Message displayed is then adjusted based on correctness
+  of input.
+*/
 if($checkEmail != 0)
 {
     $arrayu = mysqli_fetch_array($exeSQL);
-    if($arrayu['UserPw'] != $UserPw)
+    if($arrayu['UserPW'] != $UserPW)
     {
         $Message = "pw WRONG";
     }
