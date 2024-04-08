dd8a655fd2d3ce7b2b530d726d2d7f879b277eb4
JordanPinckney <157853781+JordanPinckney@users.noreply.github.com>
2024-04-07 22:21:02 -0400

    Add files via upload
    Added the map feature

--- components/organization_using_app/make_cleanup_live.js
+++ components/organization_using_app/make_cleanup_live.js
@@ -1,40 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, TextInput } from 'react-native';
 import React, { useState } from 'react';
-import { SafeAreaView, TextInput, Alert } from 'react-native';
-import { background, format, submit_button_text } from "../Features/Design.js"
+import { background, format, submit_button_text } from "../Features/Design.js";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
-/*Page you're taken to as an organization, allows you to set radius of the location, and allows for you to start/share the 
-cleanup, you can also click the volunteers button to view more data on the volunteers, you can also kick them out
-*/
+/*This page will allow organizations to set the location and radius for the cleanup*/
+
 let Make_Cleanup_Live_Org = ({navigation}) => {
 
-    const volunteers = 20;//placeholder number
+    const volunteers = 20; //placeholder number
+
+    const [region, setRegion] = useState({
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
+    });
+
+    const handleLocationSelect = (data, details) => {
+        
+        const {geometry} = details;
+        const {location} = geometry;
+
+        setRegion({
+            latitude: location.lat,
+            longitude: location.lng,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05,
+        });
+
+
+    };
     return (
         <View style={background}>
-            <View style={styles.map}>
-                <Text>map will go here</Text>
+
+            <View style={{ zIndex: 999, top: '1.3%', width: '89.2%', alignSelf: 'center', position: 'absolute', backgroundColor: 'white', borderRadius: 10 }}>
+                <GooglePlacesAutocomplete
+                    placeholder='Search'
+                    fetchDetails={true}
+                    onPress={(data, details = null) => handleLocationSelect(data, details)}                
+                    query={{
+                        key: 'AIzaSyBjOvFTHAV1-ZDMkh0YXzU-mvD32zqUiBs',
+                        language: 'en'
+                    }}
+                    style={{
+                        flex: 1,
+                        zIndex: 1,
+                        backgroundColor: 'white',
+                    }}
+                    listViewDisplayed='auto'
+                    listStyle={{flex: 1, backgroundColor: 'white'}}
+                />   
             </View>
+
+            <View style={styles.mapContainer}>
+                <MapView style={styles.map} 
+                    provider={PROVIDER_GOOGLE}
+                    initialRegion={region}
+                    region={region}
+                />
+            </View>
+
             <TouchableOpacity 
-                style={styles.volunteer_button} //this button will let you view how many volunteers have joined
+                style={[styles.buttons, styles.volunteer_button]}
                 onPress={() => navigation.navigate("Volunteers Join")}
             >
                 <View style={format}>
                     <Text style={submit_button_text}>{volunteers} VOLUNTEER(S)</Text>
                 </View>
             </TouchableOpacity>
-            <Text>Remember to wait for volunteers before pressing start!</Text>
+            
+
+            
             <TouchableOpacity 
-                style={styles.buttons} //this button will be linked to acutally starting the cleanup
+                style={styles.buttons}
             >
                 <View style={format}>
-                    
                     <Text style={submit_button_text}>START</Text>
                 </View>
             </TouchableOpacity>
 
             <TouchableOpacity 
-                style={styles.buttons} //this button will be linked to a seperate page
+                style={styles.buttons}
             >
                 <View style={format}>
                     <Text style={submit_button_text}>SHARE</Text>
@@ -54,28 +102,30 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         backgroundColor: '#efca66',
         alignSelf: 'center',
-        marginTop: 10,
+        marginTop: 0,
+        marginBottom: '10%',
         borderRadius: 10
     },
-      volunteer_button: {
+    volunteer_button: {
         backgroundColor: '#9ab880',
         width: '60%',
         height: 40,
         borderWidth: 1,
         alignSelf: 'center',
         borderRadius: 10,
-        marginTop: 10,
-        marginBottom: 20
-      },
-
-      map: {
-        width: '80%',
-        height: '45%',
+        marginBottom: '5%',
+    },
+    mapContainer: {
+        flex: 1,
+        height: '50%', 
+        marginTop: '1.1%', 
+        marginBottom: '20%', 
+        width: '89.7%',
+        borderRadius: 5,
         borderWidth: 1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        marginBottom: 15,
-        marginTop: '15%'
-      }
-    
+        zIndex: 1,
+    },
+    map: {
+        flex: 1
+    }
 });
