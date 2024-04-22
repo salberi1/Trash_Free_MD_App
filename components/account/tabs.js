7a9cf7e22d3eba7d0607e77bbb7ee9430d744276
JordanPinckney <157853781+JordanPinckney@users.noreply.github.com>
2024-04-22 15:10:15 -0400

    Navigation
    The navigation update for hosting an individual cleanup

--- components/account/tabs.js
+++ components/account/tabs.js
@@ -1,27 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TouchableOpacity, SafeAreaView, TextInput } from 'react-native';
-import React, { useState } from 'react';
-import { MultipleSelectList, SelectList} from 'react-native-dropdown-select-list'
-import { background, heading, format, make_cleanup_live_button, yes_no } from "../Features/Design.js"
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { TabActions } from '@react-navigation/native';
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 import Account from './bottom_tabs_screens/accounts';
 import Settings from './bottom_tabs_screens/settings';
 import History from './bottom_tabs_screens/history.js';
 import Join_Individual from './bottom_tabs_screens/join_cleanup.js';
-
-
-//this is going to hold the tabs of the home page for the first tier of accounts
-//the tabs are at the bottom of the screen
+import Start_Own_Cleanup from './bottom_tabs_screens/start_cleanup/start_own_cleanup.js';
+import Map_Count from './bottom_tabs_screens/start_cleanup/map_count.js';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-export default function Tabs(){
-    return(
+function StartCleanupStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Start Own Cleanup" component={Start_Own_Cleanup} />
+            <Stack.Screen name="Map Count" component={Map_Count} />
+        </Stack.Navigator>
+    );
+}
+
+function MainTabs() {
+    return (
         <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen name="Join Cleanup" component={ Join_Individual }/>
-            <Tab.Screen name="History" component= { History }/>
+            <Tab.Screen name="Join Cleanup" component={Join_Individual} />
+            <Tab.Screen name="History" component={History} />
+            <Tab.Screen name="Start Cleanup" component={StartCleanupStack} />
         </Tab.Navigator>
     );
-    
+}
+
+export default function App() {
+    return (
+        <NavigationContainer independent={true}>
+            <MainTabs />
+        </NavigationContainer>
+    );
 }
